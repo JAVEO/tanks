76bd82156b1b31f0f2f329f532155ee079e68961
Daniel Chrusciak <daniel.chrusciak@javart.eu>
2014-10-02 15:31:47 +0200

    Tiled map renderer

--- core/src/eu/javeo/tanks/TanksGame.java
+++ core/src/eu/javeo/tanks/TanksGame.java
@@ -3,8 +3,12 @@ package eu.javeo.tanks;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 
@@ -12,13 +16,23 @@ public class TanksGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
     Stage stage;
-	
+    OrthographicCamera camera;
+    OrthoCachedTiledMapRenderer tiledMapRenderer;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+
         StretchViewport viewport = new StretchViewport(480, 320);
         stage = new Stage(viewport, batch);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.update();
+        TiledMap tiledMap = new TmxMapLoader().load("levels/level1.tmx");
+        OrthoCachedTiledMapRenderer tiledMapRenderer = new OrthoCachedTiledMapRenderer(tiledMap);
     }
 
 	@Override
@@ -28,6 +42,10 @@ public class TanksGame extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+
+        camera.update();
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
 
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
